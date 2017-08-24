beb66463594a42307609ae2dcc0ba5f08839e65e
jbur4556 <jbur4556@gmail.com>
2017-08-24 17:50:50 -0600

    PlayerMovement and Camera Movement Added

---
+++ NickDosentKnow.01/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+
+    public Vector3 offset;
+    //Zoom Veriables
+    private float zoom = 10f;
+    public float zoomMax = 20f;
+    public float zoomMin = 5f;
+    public float zoomSpeed = 3f;
+    //Yaw Veriables
+    private float yawInput;
+    public float yawSpeed = 100f;
+
+    private void Update()
+    {
+        zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+
+        yawInput -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.position - offset * zoom;
+        transform.LookAt(player);
+        transform.RotateAround(player.position, Vector3.up, yawInput);
+    }
+
+
+}
---
+++ NickDosentKnow.01/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class Player_Controller : MonoBehaviour {
+    //References
+    private Rigidbody rb;
+    private Camera cam;
+    public LayerMask moveMask;
+    public LayerMask interactMask;
+    PlayerMotor motor;
+
+    //standard veriables
+    private Vector3 clickPosistion;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100f, moveMask))
+            {
+               motor.MoveToPoint(hit.point);
+            }
+        }
+    }
+}
