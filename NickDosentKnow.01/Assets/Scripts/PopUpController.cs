90446bb9a1914d5f06417207c034a556e30dd78f
jbur4556 <jbur4556@gmail.com>
2017-08-27 12:10:32 -0600

    button on interactable

---
+++ NickDosentKnow.01/Assets/Scripts/PopUpController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpController : MonoBehaviour {
+
+    public GameObject parent;
+    public Vector2 offset;
+    private RectTransform rt;
+    private Camera cam;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = Camera.main;
+        rt = GetComponent<RectTransform>();
+       // offset = RectTransformUtility.WorldToScreenPoint(cam, parent.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (parent != null)
+        {
+            Vector2 pos = RectTransformUtility.WorldToScreenPoint(cam,parent.transform.position);
+            rt.position = pos + offset; 
+        }
+	}
+}
