7529e0891340f092c522c6e44d73d3833ab47c5c
jbur4556 <jbur4556@gmail.com>
2017-08-31 14:04:43 -0600

    added spawner

--- NickDosentKnow.01/Assets/Scripts/CameraController.cs
+++ NickDosentKnow.01/Assets/Scripts/CameraController.cs
@@ -15,6 +15,12 @@ public class CameraController : MonoBehaviour {
     //Yaw Veriables
     private float yawInput;
     public float yawSpeed = 100f;
+  
+    //Pitch Variables
+    private float pitchInput;
+    public float pitchSpeed = 100f;
+    public float pitchMin;
+    public float pitchMax;
 
     private void Update()
     {
@@ -22,13 +28,19 @@ public class CameraController : MonoBehaviour {
         zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
 
         yawInput -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+        pitchInput += Input.GetAxis("Vertical") * pitchSpeed * Time.deltaTime;
+        pitchInput = Mathf.Clamp(pitchInput, pitchMin, pitchMax);
+        
     }
 
     private void LateUpdate()
     {
         transform.position = player.position - offset * zoom;
-        transform.LookAt(player);
+        Vector3 point = new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z);
+        transform.LookAt(point);
         transform.RotateAround(player.position, Vector3.up, yawInput);
+        Vector3 localX = transform.TransformDirection(Vector3.right);
+        transform.RotateAround(player.position, localX, pitchInput);
     }
 
 
---
+++ NickDosentKnow.01/Assets/Scripts/NodeSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeSpawner : MonoBehaviour {
+
+    private GameObject ground;
+    public GameObject nodePre;
+
+    private float groundX;
+    private float xMax;
+    private float xMin;
+    private float groundY;
+    private float yMax;
+    private float yMin;
+    private float groundZ;
+    private float zMax;
+    private float zMin;
+
+    private Vector3 spawnPoint;
+
+    private void Start()
+    {
+        ground = GameObject.FindGameObjectWithTag("Ground");
+
+        groundX = ground.GetComponent<Renderer>().bounds.size.x;
+        groundY = ground.GetComponent<Renderer>().bounds.size.y;
+        groundZ = ground.GetComponent<Renderer>().bounds.size.z;
+
+        InvokeRepeating("spawnNode",0.0f,1f);
+    }
+
+    void spawnNode ()
+    {
+        spawnPoint = new Vector3(Random.Range(0,groundX/2f *.8f),1, Random.Range(0,groundZ/2 *.8f));
+        Debug.Log(spawnPoint);
+        Instantiate(nodePre,spawnPoint, Quaternion.Euler(0, 0, 0), null);
+    }
+}
