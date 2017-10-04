328686356072c509cd9a3f1093091217d476ad0c
jbur4556 <jbur4556@gmail.com>
2017-10-04 13:09:48 -0600

    added construction placeholder

---
+++ NickDosentKnow.01/Assets/Scripts/Construction/BarrConstruction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrConstruction : MonoBehaviour {
+    public GameObject player;
+    private float minDistance = 5f;
+    public GameObject barrPre;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(player.transform.position,transform.position)>minDistance)
+        {
+            GameObject go = Instantiate(barrPre, transform.position, Quaternion.identity, null);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
